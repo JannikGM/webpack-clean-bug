8ee010409cbfce30efbfd27cd497f702f43ca73b
Jannik <72194488+JannikGM@users.noreply.github.com>
2021-11-10 13:34:12 +0100

    Reproduction repository for webpack-clean-bug

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: {
+            dry: false
+        }
+    },
+};
